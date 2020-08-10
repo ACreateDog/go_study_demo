6eae1994ee8581cbd966b91fcbd5cf3cff29df86
王飞 <2494662681@qq.com>
2020-08-10 21:40:18 +0800

    add algo

--- algo/lengthOfLongestSubstring.go
+++ algo/lengthOfLongestSubstring.go
@@ -1,40 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/judwhite/go-svc/svc"
-	"log"
-	"syscall"
-	"time"
-)
-
-type program struct {
-	t *time.Ticker
-}
-
-func (program) Init(env svc.Environment) error {
-	fmt.Println("program init...")
-	return nil
-}
-
-func (this *program) Start() error {
-	fmt.Println("start program")
-	fmt.Println(syscall.Getpid())
-	go func() {
-		t := time.NewTicker(2 * time.Second)
-		this.t = t
-		for d := range t.C {
-			fmt.Println("tick at ", d)
-		}
-	}()
-	return nil
-}
-
-func (this *program) Stop() error {
-	this.t.Stop()
-	fmt.Println("program stop ....")
-	return nil
-}
 func lengthOfLongestSubstringV1(s string) int {
 	if len(s) <= 0 {
 		return 0
@@ -93,14 +58,4 @@ func lengthOfLongestSubstring(s string) int { //双指针滑动窗口
 		}
 	}
 	return max
-}
-func main() {
-	var str = "aab"
-	fmt.Println(lengthOfLongestSubstring(str))
-
-	return
-	p := &program{}
-	if err := svc.Run(p, syscall.SIGUSR1, syscall.SIGINT, syscall.SIGTERM, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGTSTP); err != nil {
-		log.Fatal(err)
-	}
 }
